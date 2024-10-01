4ee1a5f6c3f1508e678c6e9423d2865f230e6575
yerzhan <erekefl@gmail.com>
2024-10-01 12:30:04 +0500

    .

---
+++ src/main/java/com/example/CRUDApplication/util/StringUtil.java
@@ -0,0 +1,8 @@
+package com.example.CRUDApplication.util;
+
+public class StringUtil {
+
+    public static boolean stringIsNullOrEmpty(String txt){
+        return txt.isEmpty() || txt.isBlank();
+    }
+}
